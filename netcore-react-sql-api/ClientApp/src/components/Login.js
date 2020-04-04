6a7ce237c1016529b801487bd9fc8881dca613e0
messidagod <joeshakely@gmail.com>
2020-04-04 14:52:12 -0700

    formatting

--- netcore-react-sql-api/ClientApp/src/components/Login.js
+++ netcore-react-sql-api/ClientApp/src/components/Login.js
@@ -1,32 +1,16 @@
-import React, { Component } from 'react';
-import "./Login.css";
+import React, { Component } from "react";
 
 import PasswordInput2 from "./PasswordInput2";
 
 export class Login extends Component {
+  static displayName = Login.name;
 
-static displayName = Login.name;
-
-  constructor(props) {
-    super(props);
-  }      
-
-    render() {
-        return (
-            <form id="Form1" method="post">
-                <div id="wrapper">
-                    <div id="innerwrapper">
-                        <div id="logincontainer columnwidth46">
-                            <div id="ctmcontentcontainer">
-                                <PasswordInput2 />
-                                <li>
-                                    <input type="submit" name="login" value="Login" />
-                                </li>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </form>
-        );
-    }
+  render() {
+    return (
+      <form id="Form1" method="post">
+        <PasswordInput2 />
+        <button className="btn btn-primary">Login</button>
+      </form>
+    );
+  }
 }
